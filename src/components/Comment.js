13a7c2846005fb14ce9bdcfb81e08415c3281778
Wiljam Rautiainen <wiljam@Wiljam-MacBook-Pro.local>
2019-01-11 18:48:51 +0200

    Save before ejecting

---
+++ src/components/Comment.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import classes from './Comment.css';
+
+class Comment extends Component {
+  render() {
+    return (
+      <div className = {classes.box}>
+      <p> boksi </p>
+      </div>
+    );
+  }
+}
+
+export default Comment;
